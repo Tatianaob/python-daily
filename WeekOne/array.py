98a4214d23e156876b1cc8ac971180931567ac41
Tatianaob <ltatiobandore@gmail.com>
2025-02-25 21:07:49 -0800

    array solution

--- WeekOne/array.py
+++ WeekOne/array.py
@@ -6,7 +6,15 @@
 # Output: [5, 4, 3, 2, 1]
 
 my_array = [1,2,3,4,5,5]
-
 reversed_array = my_array[::-1]
 print(reversed_array)
+
+### second approach
+new_array =[1,2,3,4,5,6,7,8]
+my_array_2 = []
+for i in range(len(new_array)-1,-1,-1):
+    my_array_2.append(new_array[i])
+print(my_array_2)
+
+
 
