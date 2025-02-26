22d04c81be85b7c4870fe987338a2c3af841df16
Tatianaob <ltatiobandore@gmail.com>
2025-02-25 20:47:37 -0800

    array solution

---
+++ array.py
@@ -0,0 +1,12 @@
+# Problem 1: Reverse an Array (Easy)
+# Given an array, reverse the order of its elements.
+
+# Example:
+# Input: nums = [1, 2, 3, 4, 5]
+# Output: [5, 4, 3, 2, 1]
+
+my_array = [1,2,3,4,5,5]
+
+reversed_array = my_array[::-1]
+print(reversed_array)
+
