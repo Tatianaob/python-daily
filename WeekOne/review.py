06ed4a33df698ceafec6dbc9f79b809aed8c2f2c
Tatianaob <ltatiobandore@gmail.com>
2025-06-12 16:11:55 -0700

    Loop review

--- WeekOne/review.py
+++ WeekOne/review.py
@@ -1,3 +1,4 @@
+# Variable Operations:
 def exercise_1():
     my_name =  "Tatiana Obando"
     my_age = 33
@@ -20,3 +21,28 @@ def exercise_1():
 
 exercise_1()
   
+# Loops:
+def exercise_2():
+    print("Numbers 1 to 10:")
+    for i in range(1, 11):
+      print(i, end=" ")
+    print()
+
+    # print even numbers from 1 to 20:
+    num = 2
+    while num <= 20:
+       print(num, end=" ")
+       num += 2
+    print()
+
+    sentence = "Python is awesome for programming, I love it"
+    vowels = "aeiouAEIOU"
+
+    vowel_count = 0 
+    for char in sentence:
+       if char in vowels:
+          vowel_count += 1
+    print(f"El numero de las vocales en '{sentence}' son {vowel_count}")
+
+exercise_2()
+
