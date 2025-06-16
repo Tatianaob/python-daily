87ec63bd26118910dd91018703e8ea7bda9cf265
Tatianaob <ltatiobandore@gmail.com>
2025-06-15 22:20:04 -0700

    Hashmap and set problem statement

---
+++ WeekOne/hashMap.py
@@ -0,0 +1,11 @@
+# Problem to Solve: Two Sum II (All Unique Pairs)
+# Problem Statement:
+# Given an integer array nums and an integer target, 
+# return all unique pairs of numbers that add up to the target.
+
+# Return the actual numbers (not indices). 
+# You may not return duplicate pairs.
+
+# Input:  nums = [2, 4, 3, 5, 7, 8, 1, 9]
+# target = 10
+# Expected Output: [(2, 8), (3, 7), (1, 9)]
